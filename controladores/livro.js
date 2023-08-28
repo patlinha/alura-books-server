0ecd1dc351a3d6986d03eddbe0d430289b8ea892
patricia <areiasteste@gmail.com>
2023-08-28 15:04:11 -0300

    todas as APIs - fim

--- controladores/livro.js
+++ controladores/livro.js
@@ -13,8 +13,14 @@ function getLivros(req, res){
 function getLivro(req, res){
     try {
         const id = req.params.id
-        const livro = getLivroPorId(id)
-        res.send(livro)
+
+        if(id && Number(id)) {
+            const livro = getLivroPorId(id)
+            res.send(livro)
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -24,9 +30,14 @@ function getLivro(req, res){
 function postLivro(req, res) {
     try {
         const livroNovo = req.body
-        insereLivro(livroNovo)
-        res.status(201)
-        res.send("Livro inserido com sucesso")
+        if(req.body.nome && req.body.id){
+            insereLivro(livroNovo)
+            res.status(201)
+            res.send("Livro inserido com sucesso")
+        } else {
+            res.status(422)
+            res.send("os campos 'nome' e 'id' são obrigatórios")
+        }
     } catch (error) {
         res.status(500)
         res.send(error.message)
@@ -36,21 +47,31 @@ function postLivro(req, res) {
 function patchLivro(req, res) {
     try {
         const id = req.params.id
-        const body = req.body
-
-        modificaLivro(body, id)
-        res.send("Item modificado com sucesso")
+        if(id && Number(id)) {
+            const body = req.body
+            modificaLivro(body, id)
+            res.send("Item modificado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        } 
     } catch(error) {
         res.status(500)
         res.send(error.message)
     }
+
 }
 
 function deleteLivro(req, res) {
     try {
         const id = req.params.id
-        deletaLivroPorId(id)
-        res.send("Item deletado com sucesso")
+        if(id && Number(id)) {
+            deletaLivroPorId(id)
+            res.send("Item deletado com sucesso")
+        } else {
+            res.status(422)
+            res.send("Id inválido")
+        } 
     } catch(error) {
         res.status(500)
         res.send(error.message)
